2589cc327554c81e61837b30b87939855af45c50
Rowland Adimoha <rowlives@gmail.com>
2025-04-21 12:08:48 +0100

    chore: delete user management

--- services/user-management/src/services/index.ts
+++
@@ -1,9 +0,0 @@
-import { nats } from '../app';
-import { redis } from '../config/redis';
-import { UserService } from './userService';
-
-// Initialize services with dependencies
-export const userService = new UserService(redis, nats);
-
-// Export service types
-export type { UserService };
