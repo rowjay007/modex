c5a53781a39a40b8e28065dd6353088ad9d90179
Rowland Adimoha <rowlives@gmail.com>
2025-05-28 16:44:25 +0100

    chore: remove static files from logger

--- services/user-management/src/app.ts
+++ services/user-management/src/app.ts
@@ -16,7 +16,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  logger.info(`${req.method} ${req.url}`);
+  if (!req.url.startsWith('/docs')) {
+    logger.info(`${req.method} ${req.url}`);
+  }
   next();
 });
 
